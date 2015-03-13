2bf3ee5f100c0d9c30e0d80bbe6602f9e9250222
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-03-13 14:58:32 +0530

    Update basic setup

--- package.js
+++ package.js
@@ -3,13 +3,27 @@ Package.describe({
   name: "meteorhacks:firewall"
 });
 
+Npm.depends({
+  "request": "2.53.0",
+  "cookies": "0.5.0"
+});
+
 Package.on_use(function (api, where) {
   api.versionsFrom("METEOR@0.9.0");
-  api.use(['meteorhacks:meteorx@1.3.1', 'underscore']);
+  api.use([
+    'underscore',
+    'meteorhacks:meteorx@1.3.1',
+    'chuangbo:cookie@1.1.0'
+  ]);
+
   api.add_files([
     'lib/server/core.js',
     'lib/server/logic.js'
   ], 'server');
+
+  api.add_files([
+    'lib/client/token.js'
+  ], 'client');
 
   api.add_files([
     'lib/server/captcha_page.html'
